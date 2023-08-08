e03eaa6134d716515a389c3efb64c6934d103da4
Yoiz Nuñez <72285660+YoizNunez@users.noreply.github.com>
2023-08-08 07:23:52 -0600

    Update ANN.py

--- mmWave-Outdoor/ANN.py
+++ mmWave-Outdoor/ANN.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-import numpy as np
 import pandas as pd
 import seaborn as sns
 from tqdm.notebook import tqdm
