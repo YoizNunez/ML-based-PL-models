bc4a5068c07d5f19bbb27f1e950bc20cfcf01352
Yoiz Nuñez <72285660+YoizNunez@users.noreply.github.com>
2023-08-08 09:32:21 -0600

    Update GTB - Predictor Coalition.py

--- V2I/GTB - Predictor Coalition.py
+++ V2I/GTB - Predictor Coalition.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 12 10:24:22 2023
-@author: Yoiz Nuñez
-"""
-
-#1 Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from tqdm.notebook import tqdm
-import matplotlib.pyplot as plt
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader, ConcatDataset
-from torch.utils.data import SubsetRandomSampler #split the dataset
 
 from sklearn.preprocessing import MinMaxScaler    
 from sklearn.model_selection import train_test_split
@@ -40,13 +22,6 @@ import matplotlib.pyplot as plt
 import tabulate
 from tabulate import tabulate
 
-# check xgboost version
-import xgboost
-from xgboost import XGBRegressor
-
-torch.manual_seed(0)
-np.random.seed(0)
-
 import random
 random.seed(0)
 
@@ -54,59 +29,51 @@ random.seed(0)
 """
 Reading the CSV files
 """
+#Route1
+path=r"Route1_735.csv"
+df_R1_735 = pd.read_csv(path)
+df_R1_735.head()
 
-#SC15
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\V2I\Dataset Banda Larga\SC_15.csv"
-df_SC15 = pd.read_csv(path)
-df_SC15.head()
+path=r"Route1_2540.csv"
+df_R1_2540 = pd.read_csv(path)
+df_R1_2540.head()
 
-#SC19
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\V2I\Dataset Banda Larga\SC_19.csv"
-df_SC19 = pd.read_csv(path)
-df_SC19.head()
+path=r"Route1_3500.csv"
+df_R1_3500 = pd.read_csv(path)
+df_R1_3500.head()
 
-#SC20
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\V2I\Dataset Banda Larga\SC_20.csv"
-df_SC20 = pd.read_csv(path)
-df_SC20.head()
+#Route2
+path=r"Route2_735.csv"
+df_R2_735 = pd.read_csv(path)
+df_R2_735.head()
 
-#SC23
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\V2I\Dataset Banda Larga\SC_23.csv"
-df_SC23 = pd.read_csv(path)
-df_SC23.head()
+path=r"Route2_2540.csv"
+df_R2_2540 = pd.read_csv(path)
+df_R2_2540.head()
 
-#SC24
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\V2I\Dataset Banda Larga\SC_24.csv"
-df_SC24 = pd.read_csv(path)
-df_SC24.head()
-
-#SC27
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\V2I\Dataset Banda Larga\SC_27.csv"
-df_SC27 = pd.read_csv(path)
-df_SC27.head()
-
-
+path=r"Route2_3500.csv"
+df_R2_3500 = pd.read_csv(path)
+df_R2_3500.head()
 
 #%%
-
-#Total areas
+#Route1
 df_train = pd.concat([
-    df_SC23,#3.5 GHz
-    df_SC24, #2.54 GHz
-    df_SC27, #735 MHz
+    df_R1_735,
+    df_R1_2540, 
+    df_R1_3500
     ])
 
+#Route2
 df_test = pd.concat([
-    df_SC15, #735 MHz
-    df_SC19, #3.5 GHz
-    df_SC20 #2.54 GHz
+    df_R2_735, 
+    df_R2_2540, 
+    df_R2_3500 
     ])
 
 #%%
 """
 Create Input and Output Data
 """
-
 x=[6,15,11,17,12,13,7,14,10,8,9,16] #Predictors
 
 X_train = df_train.iloc[:, x]
@@ -131,7 +98,6 @@ y_test = scaler.transform(y_test)
 # convert output variable to float
 y_train, y_test = y_train.astype(float), y_test.astype(float),
 
-
 gtb_regressor = GradientBoostingRegressor(n_estimators=70, 
                                           learning_rate=0.14, 
                                           max_depth=10, 
@@ -140,7 +106,6 @@ gtb_regressor = GradientBoostingRegressor(n_estimators=70,
                                           loss='absolute_error', 
                                           max_features='auto',random_state=42) #42
 gtb_regressor.fit(X_train,np.ravel(y_train))
-
 
 #TRAINING
 y_pred = gtb_regressor.predict(X_train)
@@ -190,7 +155,6 @@ data = [['735',RMSE_735, R2_735,variance_735,mean_735]
         ]  
 print(tabulate(data, headers=["Freq",'RMSE','R^2','Variance [dB]','Mean [dB]']))
 
-
 MSE = np.square(np.subtract(y_target_desn,y_pred_desn)).mean() #RMSE
 RMSE_train = math.sqrt(MSE)
 
@@ -210,7 +174,6 @@ for x in abs_dif:
 
 SD_train = math.sqrt(sum_model/(n)) #SD
 
-
 #TESTING
 y_pred_test = gtb_regressor.predict(X_test)
 y_pred_test = y_pred_test.reshape(-1,1)
@@ -218,7 +181,6 @@ y_pred_test = y_pred_test.reshape(-1,1)
 y_pred_desn_test = scaler.inverse_transform(y_pred_test)
 
 y_target_desn_test = scaler.inverse_transform(y_test)
-
 
 pl_pred_test=y_pred_desn_test
 
@@ -263,8 +225,6 @@ data = [['735',RMSE_735, R2_735,R2_oos_735,variance_735,mean_735]
         ]  
 print(tabulate(data, headers=["Freq",'RMSE','R^2','R^2 OOS','Variance [dB]','Mean[dB]']))
 
-
-
 #Plot
 p1 = max(max(y_target_desn_test), max(y_target_desn_test))
 p2 = min(min(y_target_desn_test), min(y_target_desn_test))
@@ -279,10 +239,8 @@ plt.savefig('R2_V2I_BW.eps',format='eps',dpi=1200)
 plt.show()
 plt.close()
 
-
 MSE = np.square(np.subtract(y_target_desn_test,y_pred_desn_test)).mean()
 RMSE_test = math.sqrt(MSE)
-
 
 R2_test= r2_score(y_target_desn_test,y_pred_desn_test) #R2
 
@@ -307,7 +265,6 @@ print(tabulate(data, headers=["","RMSE ",'MAPE [%]','SD','R^2']))
 #Testing
 data = [['Testing set',RMSE_test, MAPE_test, SD_test,R2_test]]
 print(tabulate(data, headers=["","",'','','']))
-
 
 import pymint #version: 0.2.6
 import pickle
@@ -341,7 +298,6 @@ MEC = explainer_gtb.main_effect_complexity(ale_model,max_segments=10)
 IAS= explainer_gtb.interaction_strength(ale_model)
 
 #%%
-
 "All predictors"
 #predictor dist
 
@@ -375,7 +331,6 @@ ale_d_bin_s7= ale_model.d__bin_values.to_masked_array()
 
 #%%
 "6-pred"
-
 ale_f_value_s6 = ale_model.f__GTB__ale.to_masked_array()  
 ale_f_value_s6= ale_f_value_s6.ravel() #to reduce 1 dim
 ale_f_bin_s6= ale_model.f__bin_values.to_masked_array()
@@ -401,7 +356,6 @@ ale_sg_value_s6= ale_sg_value_s6.ravel() #to reduce 1 dim
 ale_sg_bin_s6= ale_model.sg__bin_values.to_masked_array()
 
 #%%
-
 "5-pred"
 ale_f_value_s5 = ale_model.f__GTB__ale.to_masked_array()  
 ale_f_value_s5= ale_f_value_s5.ravel() #to reduce 1 dim
@@ -425,7 +379,6 @@ ale_sv_bin_s5= ale_model.sv__bin_values.to_masked_array()
 
 
 #%%
-
 "4-pred"
 ale_f_value_s4 = ale_model.f__GTB__ale.to_masked_array()  
 ale_f_value_s4= ale_f_value_s4.ravel() #to reduce 1 dim
@@ -445,7 +398,6 @@ ale_nv_bin_s4= ale_model.nv__bin_values.to_masked_array()
 
 
 #%%
-
 "3-pred"
 ale_f_value_s3 = ale_model.f__GTB__ale.to_masked_array()  
 ale_f_value_s3= ale_f_value_s3.ravel() #to reduce 1 dim
@@ -460,7 +412,6 @@ ale_sb_value_s3= ale_sb_value_s3.ravel() #to reduce 1 dim
 ale_sb_bin_s3= ale_model.sb__bin_values.to_masked_array()
 
 #%%
-
 "2-pred"
 ale_f_value_s2 = ale_model.f__GTB__ale.to_masked_array()  
 ale_f_value_s2= ale_f_value_s2.ravel() #to reduce 1 dim
@@ -471,169 +422,12 @@ ale_nb_value_s2= ale_nb_value_s2.ravel() #to reduce 1 dim
 ale_nb_bin_s2= ale_model.nb__bin_values.to_masked_array()
 
 #%%
-
 "1-pred"
 ale_f_value_s1 = ale_model.f__GTB__ale.to_masked_array()  
 ale_f_value_s1= ale_f_value_s1.ravel() #to reduce 1 dim
 ale_f_bin_s1= ale_model.f__bin_values.to_masked_array()
 
 #%%
-
-import matplotlib.gridspec as gridspec
-
-
-fig = plt.figure(figsize=(25,20))
-spec = gridspec.GridSpec(ncols=3, nrows=3) # 6 columns evenly divides both 2 & 3
-
-ax1 = fig.add_subplot(spec[0,0]) # row 0 with axes spanning 2 cols on evens
-ax2 = fig.add_subplot(spec[0,1]) # row 0 with axes spanning 2 cols on evens
-ax3 = fig.add_subplot(spec[0,2]) # row 0 with axes spanning 2 cols on evens
-ax4 = fig.add_subplot(spec[1,0]) # row 0 with axes spanning 2 cols on evens
-ax5 = fig.add_subplot(spec[1,1]) # row 0 with axes spanning 2 cols on evens
-ax6 = fig.add_subplot(spec[1,2]) # row 0 with axes spanning 2 cols on evens
-ax7 = fig.add_subplot(spec[2,0]) # row 0 with axes spanning 2 cols on evens
-
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\Plot - ALE - V2I\ALE-f.csv"
-df = pd.read_csv(path)
-
-#f
-ax1.plot(df['ale_f_bin_s1'],df['ale_f_value_s1'],"+-", label ='1',lw=6,color='blue')
-ax1.plot(df['ale_f_bin_s2'],df['ale_f_value_s2'],"-.", label ='2',lw=6,color='red')
-ax1.plot(df['ale_f_bin_s3'],df['ale_f_value_s3'],"--", label ='3',lw=6,color='green')
-ax1.plot(df['ale_f_bin_s4'],df['ale_f_value_s4'],":",label ='4',lw=6,color='orange')
-ax1.plot(df['ale_f_bin_s5'],df['ale_f_value_s5'],"x-", label ='5',lw=6,color='brown')
-ax1.plot(df['ale_f_bin_s6'],df['ale_f_value_s6'], label ='6',lw=6,color='purple')
-ax1.plot(df['ale_f_bin_s7'],df['ale_f_value_s7'], label ='7',lw=6,color='black')
-ax1.grid(ls='--')
-ax1.set_xlabel('$f$ (GHz)',fontsize=32)
-ax1.set_title('(a)',size=32)
-#ax1.set_ylabel('(a)',fontsize=18)
-ax1.xaxis.set_tick_params(labelsize=32)
-ax1.yaxis.set_tick_params(labelsize=32)
-ax1.legend(fancybox=False,shadow=False,fontsize="20")
-x_labels1 = ['','0.73','2.5','3.5']
-ax1.set_xticklabels(x_labels1)
-    
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\Plot - ALE - V2I\ALE-nb.csv"
-df = pd.read_csv(path)
-
-#nb
-ax2.plot(df['ale_nb_bin_s2'],df['ale_nb_value_s2'],"-.", label ='2',lw=6,color='red')
-ax2.plot(df['ale_nb_bin_s3'],df['ale_nb_value_s3'],"-.", label ='3',lw=6,color='green')
-ax2.plot(df['ale_nb_bin_s4'],df['ale_nb_value_s4'],"--", label ='4',lw=6,color='orange')
-ax2.plot(df['ale_nb_bin_s5'],df['ale_nb_value_s5'],":", label ='5',lw=6,color='brown')
-ax2.plot(df['ale_nb_bin_s6'],df['ale_nb_value_s6'],"x-", label ='6',lw=6,color='purple')
-ax2.plot(df['ale_nb_bin_s7'],df['ale_nb_value_s7'], label ='7',lw=6,color='black')
-ax2.set_title('(b)',size=32)
-ax2.grid(ls='--')
-ax2.set_xlabel('$n_b$',fontsize=32)
-#ax2.set_ylabel('(b)',fontsize=18)
-ax2.xaxis.set_tick_params(labelsize=32)
-ax2.yaxis.set_tick_params(labelsize=32)
-ax2.legend(fancybox=False,shadow=False,fontsize="20")
-x_labels2 = ['4','8','12','16']
-ax2.set_xticklabels(x_labels2)
-
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\Plot - ALE - V2I\ALE-sb.csv"
-df = pd.read_csv(path)
-#sb
-ax3.plot(df['ale_sb_bin_s3'],df['ale_sb_value_s3'],"--", label ='3',lw=6,color='green')
-ax3.plot(df['ale_sb_bin_s4'],df['ale_sb_value_s4'],"--", label ='4',lw=6,color='orange')
-ax3.plot(df['ale_sb_bin_s5'],df['ale_sb_value_s5'],":", label ='5',lw=6,color='brown')
-ax3.plot(df['ale_sb_bin_s6'],df['ale_sb_value_s6'],"x-", label ='6',lw=6,color='purple')
-ax3.plot(df['ale_sb_bin_s7'],df['ale_sb_value_s7'], label ='7',lw=6,color='black')
-ax3.grid(ls='--')
-ax3.set_xlabel('$\sigma_b$ (m)',fontsize=32)
-ax3.set_title('(c)',size=32)
-#ax3.set_ylabel('(c)',fontsize=18)
-ax3.xaxis.set_tick_params(labelsize=32)
-ax3.yaxis.set_tick_params(labelsize=32)
-ax3.legend(fancybox=False,shadow=False,fontsize="20")
-x_labels3 = ['2.17','6.29','10.41','14.54','18.66']
-ax3.set_xticklabels(x_labels3)
-
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\Plot - ALE - V2I\ALE-nv.csv"
-df = pd.read_csv(path)
-
-ax4.plot(df['ale_nv_bin_s4'],df['ale_nv_value_s4'],":", label ='4',lw=6,color='orange')
-ax4.plot(df['ale_nv_bin_s5'],df['ale_nv_value_s5'],":", label ='5',lw=6,color='brown')
-ax4.plot(df['ale_nv_bin_s6'],df['ale_nv_value_s6'],"x-", label ='6',lw=6,color='purple')
-ax4.plot(df['ale_nv_bin_s7'],df['ale_nv_value_s7'], label ='7',lw=6,color='black')
-ax4.grid(ls='--')
-ax4.set_title('(d)',size=32)
-ax4.set_xlabel('$n_v$',fontsize=32)
-#ax4.set_ylabel('(d)',fontsize=18)
-ax4.xaxis.set_tick_params(labelsize=32)
-ax4.yaxis.set_tick_params(labelsize=32)
-ax4.legend(fancybox=False,shadow=False,fontsize="20")
-x_labels4 = ['0','2','4','6','8']
-ax4.set_xticklabels(x_labels4)
-
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\Plot - ALE - V2I\ALE-sv.csv"
-df = pd.read_csv(path)
-
-ax5.plot(df['ale_sv_bin_s5'],df['ale_sv_value_s5'],"x-", label ='5',lw=6,color='brown')
-ax5.plot(df['ale_sv_bin_s6'],df['ale_sv_value_s6'],"x-", label ='6',lw=6,color='purple')
-ax5.plot(df['ale_sv_bin_s7'],df['ale_sv_value_s7'], label ='7',lw=6,color='black')
-ax5.grid(ls='--')
-ax5.set_xlabel('$\sigma_v$ (m)',fontsize=32)
-#ax5.set_ylabel('(e)',fontsize=18)
-ax5.set_title('(e)',size=32)
-ax5.xaxis.set_tick_params(labelsize=32)
-ax5.yaxis.set_tick_params(labelsize=32)
-ax5.legend(fancybox=False,shadow=False,fontsize="20")
-x_labels5 = ['0','2.95','5.90','8.85','11.81']
-ax5.set_xticklabels(x_labels5)
-
-
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\Plot - ALE - V2I\ALE-sg.csv"
-df = pd.read_csv(path)
-
-ax6.plot(df['ale_sg_bin_s6'],df['ale_sg_value_s6'], label ='6',lw=6,color='purple')
-ax6.plot(df['ale_sg_bin_s7'],df['ale_sg_value_s7'], label ='7',lw=6,color='black')
-ax6.grid(ls='--')
-ax6.set_xlabel('$\sigma_g$ (m)',fontsize=32)
-#ax6.set_ylabel('(f)',fontsize=18)
-ax6.xaxis.set_tick_params(labelsize=32)
-ax6.yaxis.set_tick_params(labelsize=32)
-ax6.set_title('(f)',size=32)
-ax6.legend(fancybox=False,shadow=False,fontsize="20")
-x_labels6 = ['0.82','1.63','2.45','3.26']
-ax6.set_xticklabels(x_labels6)
-
-
-path=r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\Plot - ALE - V2I\ALE-d.csv"
-df = pd.read_csv(path)
-
-ax7.plot(df['ale_d_bin_s7'],df['ale_d_value_s7'], label ='7',lw=6,color='black')
-ax7.grid(ls='--')
-ax7.set_xlabel('$d$ (m)',fontsize=32)
-ax7.xaxis.set_tick_params(labelsize=32)
-ax7.yaxis.set_tick_params(labelsize=32)
-ax7.legend(fancybox=False,shadow=False,fontsize="20")
-ax7.set_title('(g)',size=32)
-#x_labels4 = ['15','24.5','34.0','43.5','53.0']
-#ax4.set_xticklabels(x_labels4)
-x_labels7 = ['69.8','138.4','206.9','275.5']
-ax7.set_xticklabels(x_labels7)
-
-
-plt.subplots_adjust(left=0.1,
-                    bottom=0.1,
-                    right=0.9,
-                    top=0.9,
-                    wspace=0.3,
-                    hspace=0.45
-                    )
-
-#fig.tight_layout()
-plt.savefig('ALE-GTB-V2I.eps',format='eps',dpi=600)
-plt.show()
-plt.close()
-
-
-#%%
-
 #PFI
 from sklearn.inspection import permutation_importance
 
@@ -641,11 +435,9 @@ r = permutation_importance(gtb_regressor, X_train, y_train,
                            n_repeats=10,
                            random_state=0)
 
-
 X_train = df_train.iloc[:, [6,15,11,17,12,13,7]] 
 y_train = df_train.iloc[:, [25]]
 
-#Select the testing set: SC1 or SC2
 X_test = df_test.iloc[:, [6,15,11,17,12,13,7]] 
 y_test = df_test.iloc[:, [25]]
 
@@ -682,12 +474,10 @@ y_train = df_train.iloc[:, [25]]
 X_test = df_test.iloc[:, [6,15,11,17,12,13,7]] 
 y_test = df_test.iloc[:, [25]]
 
-
 X = df_train.iloc[:, [6,15,11,17,12,13,7]] # 12,5,7,10,4,15
 y_train = df_train.iloc[:, [25]]
 
 num_iterations=10
-
 
 scores={c:[] for c in X.columns}
 
@@ -726,7 +516,6 @@ for c in X.columns:
        scores[c].append((baseline_metric-score)*-1)
 
 #%%
-
 """
 Applying Cross-Validation
 """
@@ -745,7 +534,6 @@ p = len(num_trees)*max_iter
 zmatrix_cv_gb = np.zeros((p, 5)) #size of the cross-validation matriz
 
 cv = KFold(n_splits=5,shuffle=True,random_state=0)
-
 
 count = 0
 md = 0
