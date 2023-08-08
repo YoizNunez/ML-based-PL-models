e2d5f1c3ae91a6621bbf3f592f74289532b33e2f
Yoiz Nuñez <72285660+YoizNunez@users.noreply.github.com>
2023-08-08 07:23:33 -0600

    Update SVR.py

--- mmWave-Outdoor/SVR.py
+++ mmWave-Outdoor/SVR.py
@@ -1,44 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 12 10:24:22 2023
-@author: Yoiz Nuñez
-"""
-
-#1 Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from tqdm.notebook import tqdm
 import matplotlib.pyplot as plt
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader, ConcatDataset
-from torch.utils.data import SubsetRandomSampler #split the dataset
-
-from sklearn.preprocessing import MinMaxScaler    
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
 import math
 from sklearn.svm import SVR
-from sklearn import gaussian_process
-from sklearn.gaussian_process import kernels
 
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
 import tabulate
 from tabulate import tabulate
-
-torch.manual_seed(0)
-np.random.seed(0)
 
 import random
 random.seed(0)
@@ -47,14 +23,14 @@ random.seed(0)
 """
 Read Data
 """
-path = r"C:\Users\Yoiz Nuñez\Documents\DOUTORADO 2023\Outdoor_data_vegetationdepth_qgis_final.csv"
+path = r"outdoor.csv"
 df = pd.read_csv(path)
 df.head()
 
 """
 Create Input and Output Data
 """
-X = df.iloc[:, [0,1,2,5]] #with vegetation depth -->5 
+X = df.iloc[:, [0,1,2,5]] 
 y = df.iloc[:, [3]]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=90)
@@ -78,7 +54,6 @@ y_train, y_test = y_train.astype(float), y_test.astype(float),
 svr_regressor = SVR(kernel='rbf', C=200, epsilon=0.1,gamma=0.3)
   
 svr_regressor.fit(X_train,np.ravel(y_train))
-
 
 #TRAINING
 y_pred = svr_regressor.predict(X_train)
@@ -107,14 +82,12 @@ for x in abs_dif:
 
 SD_train = math.sqrt(sum_model/(n)) #SD
 
-
 #TESTING
 y_pred_test = svr_regressor.predict(X_test)
 y_pred_test = y_pred_test.reshape(-1,1)
 
 y_pred_desn_test = scaler.inverse_transform(y_pred_test)
 y_target_desn_test = scaler.inverse_transform(y_test)
-
 
 #Plot
 p1 = max(max(y_target_desn_test), max(y_target_desn_test))
@@ -126,7 +99,6 @@ plt.show()
 
 MSE = np.square(np.subtract(y_target_desn_test,y_pred_desn_test)).mean()
 RMSE_test = math.sqrt(MSE)
-
 
 R2_test= r2_score(y_target_desn_test,y_pred_desn_test) #R2
 
@@ -156,7 +128,6 @@ print(tabulate(data, headers=["","",'','','']))
 """
 Applying Cross-Validation
 """
-
 C = np.arange(200,2200,100) 
 epsilon= [0.005,0.05,0.1] 
 gamma=[0.1, 0.2, 0.3]
@@ -207,17 +178,3 @@ for i in range(len(epsilon)):
         matrix_cv_svr[count][2] = 0.1
         matrix_cv_svr[count][3] = RMSE_tot
         count += 1
-
-
-    
-#%%    
-import numpy
-import matplotlib.pyplot as plt
-
-
-p1 = max(max(y_target_desn_test), max(y_target_desn_test))
-p2 = min(min(y_target_desn_test), min(y_target_desn_test))
-plt.plot([p1, p2], [p1, p2], '--',linewidth=1,label= 'Best fit',c='navy')
-
-plt.scatter(y_target_desn_test,y_pred_desn_test)
-plt.show()
